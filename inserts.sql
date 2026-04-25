b5f8331fab1b2da0de75af0c1f976f766433dcfb
hsunccc <hsunho0318@gmail.com>
2026-04-24 22:13:09 -0400

    Refined tables and inserted records

--- inserts.sql
+++ inserts.sql
@@ -1,3 +1,43 @@
 INSERT INTO STUDENT VALUES ('RP031006', 'Rebecca', 'Therese', 'Post');
 INSERT INTO STUDENT VALUES ('NW050506', 'Nicole', 'Katherine', 'Wood');
-SELECT * FROM STUDENT;
+INSERT INTO Student VALUES ('S0000001', 'Hsun', 'Chih', 'Ho');
+INSERT INTO Student VALUES ('S0000002', 'Maria', 'J', 'Garcia');
+INSERT INTO Student VALUES ('S0000003', 'James', 'Michael', 'Kim');
+
+-- professors
+INSERT INTO Professor VALUES ('P0000001', 'Dr. Alan Reed', 'CS');
+INSERT INTO Professor VALUES ('P0000002', 'Dr. Susan Lane', 'MATH');
+INSERT INTO Professor VALUES ('P0000003', 'Dr. Mark Ellis', 'CS');
+
+-- assignments
+INSERT INTO Assignment VALUES ('A0000001', 'Chapter 1 HW');
+INSERT INTO Assignment VALUES ('A0000002', 'Midterm Review');
+INSERT INTO Assignment VALUES ('A0000003', 'Final Project');
+
+-- courses
+INSERT INTO Course VALUES ('10001', 'CSC4480', 'Database Systems', 'CS', 'Spring', 2026, 3, 'P0000001');
+INSERT INTO Course VALUES ('10002', 'CSC3300', 'Data Structures', 'CS', 'Spring', 2026, 3, 'P0000003');
+INSERT INTO Course VALUES ('10003', 'MATH2200', 'Discrete Math', 'MATH', 'Spring', 2026, 3, 'P0000002');
+
+-- grade_Category
+INSERT INTO Grade_Category VALUES ('GC000001', '10001', 'Participation', 10.00);
+INSERT INTO Grade_Category VALUES ('GC000002', '10001', 'HW_Average', 20.00);
+INSERT INTO Grade_Category VALUES ('GC000003', '10001', 'Tests', 50.00);
+
+-- student_Record
+INSERT INTO Student_Record VALUES ('R0000001', 'S0000001', '10001', 95, 88, 90, 85, 92, NULL, NULL, NULL, 91, 5);
+INSERT INTO Student_Record VALUES ('R0000002', 'S0000002', '10001', 80, 75, 78, 70, 82, NULL, NULL, NULL, 85, 0);
+INSERT INTO Student_Record VALUES ('R0000003', 'S0000003', '10001', 70, 65, 72, 68, 74, NULL, NULL, NULL, 78, 3);
+INSERT INTO Student_Record VALUES ('R0000004', 'RP031006', '10001', 88, 90, 85, 92, 87, NULL, NULL, NULL, 89, 0);
+INSERT INTO Student_Record VALUES ('R0000005', 'NW050506', '10001', 76, 80, 78, 74, 82, NULL, NULL, NULL, 83, 2);
+-- enrolled_In
+INSERT INTO Enrolled_In VALUES ('S0000001', '10001');
+INSERT INTO Enrolled_In VALUES ('S0000002', '10001');
+INSERT INTO Enrolled_In VALUES ('S0000003', '10002');
+INSERT INTO Enrolled_In VALUES ('RP031006', '10001');
+INSERT INTO Enrolled_In VALUES ('NW050506', '10001');
+
+-- assigns
+INSERT INTO Assigns VALUES ('A0000001', '10001');
+INSERT INTO Assigns VALUES ('A0000002', '10001');
+INSERT INTO Assigns VALUES ('A0000003', '10002');
--- tables.sql
+++ tables.sql
@@ -4,51 +4,63 @@ CREATE TABLE Student (
     M_Name varchar2(20),
     L_Name varchar2(20)
 );
+
 CREATE TABLE Professor (
     Prof_ID varchar2(8) PRIMARY KEY NOT NULL,
     Prof_Name varchar2(25),
-    Dept_ID varchar2(8), 
-    FOREIGN KEY REFERENCES Department(Dept_ID)
+    Dept_ID varchar2(10) 
 );
 
-CREATE TABLE Course (
-    CRN varchar2(8) PRIMARY KEY NOT NULL,
-    Prof_ID varchar2(8),
-    FOREIGN KEY REFERENCES Professor(Prof_ID),
-    Course_Name varchar2(15),
-    Semester varchar2(10),
-    Year int,
-    Credits int, -- MUST BE > 0
-    Dept_ID varchar2(8),
-    FOREIGN KEY REFERENCES Department(Dept_ID)
-);
-CREATE Table Assignment (
+CREATE TABLE Assignment (
     Assignment_ID varchar2(8) PRIMARY KEY NOT NULL,
     Assignment_Name varchar2(25)
+);
+CREATE TABLE Course (
+    CRN varchar2(8) PRIMARY KEY NOT NULL,
+    Course_ID varchar2(8),  
+    Course_Name varchar2(50),
+    Dept_ID varchar2(10),   
+    Semester varchar2(10),
+    Year int,
+    Credits int CHECK (Credits > 0), 
+    Prof_ID varchar2(8),
+    CONSTRAINT fk_Course_Prof FOREIGN KEY (Prof_ID) REFERENCES Professor(Prof_ID)
 );
 CREATE TABLE Student_Record (
     Record_ID varchar2(8) PRIMARY KEY NOT NULL,
     Student_ID varchar2(8),
-    FOREIGN KEY REFERENCES Student(Student_ID),
     CRN varchar2(8),
-    FOREIGN KEY REFERENCES Course(CRN),
     Participation int,
+    Quiz_Average int,
+    HW_Average int,     
     Test1 int,
     Test2 int,
-    Final_Proj int, 
-    Final_Exam int,
-    Extra_Credit int
+    Test3 int,
+    Test4 int,
+    Test5 int,
+    FinProj int,        
+    Extra_Credit int,
+    CONSTRAINT fk_StudentRecord_Student FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID),
+    CONSTRAINT fk_StudentRecord_Course FOREIGN KEY (CRN) REFERENCES Course(CRN)
 );
 CREATE TABLE Enrolled_In (
     Student_ID varchar2(8),
-    Foreign KEY REFERENCES Student(Student_ID),
     Course_ID varchar2(8),
-    FOREIGN KEY REFERENCES Course(CRN)
-);
-CREATE TABLE Enrolled_In ( --spine table
-    Student_ID varchar2(8),
+    CONSTRAINT pk_Enrolled_In PRIMARY KEY (Student_ID, Course_ID),
     CONSTRAINT fk_Enrolled_In_Student FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID),
+    CONSTRAINT fk_Enrolled_In_Course FOREIGN KEY (Course_ID) REFERENCES Course(CRN)
+);
+CREATE TABLE Assigns ( 
+    Assignment_ID varchar2(8),
     Course_ID varchar2(8),
-    CONSTRAINT fk_Enrolled_In_Course FOREIGN KEY (Course_ID) REFERENCES Course(CRN),
-    CONSTRAINT pk_Enrolled_In PRIMARY KEY (Student_ID, Course_ID)
+    CONSTRAINT pk_Assigns PRIMARY KEY (Assignment_ID, Course_ID), 
+    CONSTRAINT fk_Assigns_Assignment FOREIGN KEY (Assignment_ID) REFERENCES Assignment(Assignment_ID),
+    CONSTRAINT fk_Assigns_Course FOREIGN KEY (Course_ID) REFERENCES Course(CRN)
+);
+CREATE TABLE Grade_Category (
+    Category_ID   varchar2(8)  PRIMARY KEY NOT NULL,
+    CRN           varchar2(8),
+    Category_Name varchar2(25),
+    Weight        decimal(5,2),
+    CONSTRAINT fk_GradeCat_Course FOREIGN KEY (CRN) REFERENCES Course(CRN)
 );
