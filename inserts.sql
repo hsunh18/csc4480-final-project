a8e27e24e0291a16b858df74c972a7cce5636504
Nicole Wood <nwood01@villanova.edu>
2026-04-28 16:53:34 -0400

    adding inserts, creating some queries

--- inserts.sql
+++ inserts.sql
@@ -20,38 +20,59 @@ INSERT INTO Assignment VALUES ('A0000003', 'Quiz');
 INSERT INTO Assignment VALUES ('A0000004', 'Test 1');
 INSERT INTO Assignment VALUES ('A0000005', 'Test 2');
 INSERT INTO Assignment VALUES ('A0000006', 'Final Project');
---assigns (to databases)
+INSERT INTO Assignment VALUES ('A0000007', 'Final Exam');
+-- assigns (to databases)
 INSERT INTO Assigns VALUES ('A0000001', '23491');
 INSERT INTO Assigns VALUES ('A0000002', '23491');
 INSERT INTO Assigns VALUES ('A0000003', '23491');
 INSERT INTO Assigns VALUES ('A0000004', '23491');
-INSERT INTO Assigns VALUES ('A0000006', '23491');
---assigns (to game dev)
+INSERT INTO Assigns VALUES ('A0000005', '23491');
+INSERT INTO Assigns VALUES ('A0000007', '23491');
+-- assigns (to game dev)
 INSERT INTO Assigns VALUES ('A0000001', '64930');
 INSERT INTO Assigns VALUES ('A0000002', '64930');
 INSERT INTO Assigns VALUES ('A0000006', '64930');
---assigns (to discrete struc)
+-- assigns (to discrete struc)
 INSERT INTO Assigns VALUES ('A0000001', '39285');
 INSERT INTO Assigns VALUES ('A0000002', '39285');
 INSERT INTO Assigns VALUES ('A0000004', '39285');
 INSERT INTO Assigns VALUES ('A0000005', '39285');
+INSERT INTO Assigns VALUES ('A0000007', '39285');
 
--- grade cat
-INSERT INTO Grade_Category VALUES ('GC000001', '10001', 'Participation', 10.00);
-INSERT INTO Grade_Category VALUES ('GC000002', '10001', 'HW_Average', 20.00);
-INSERT INTO Grade_Category VALUES ('GC000003', '10001', 'Tests', 50.00);
+-- grade category (database systems)
+INSERT INTO Grade_Category VALUES ('GC000001', '23491', 'Participation', 10.00);
+INSERT INTO Grade_Category VALUES ('GC000002', '23491', 'Homework', 20.00);
+INSERT INTO Grade_Category VALUES ('GC000003', '23491', 'Quizzes', 20.00);
+INSERT INTO Grade_Category VALUES ('GC000004', '23491', 'Tests', 30.00);
+INSERT INTO Grade_Category VALUES ('GC000005', '23491', 'Final Exam', 20.00);
+-- grade category (game dev)
+INSERT INTO Grade_Category VALUES ('GC000006', '64930', 'Participation', 10.00);
+INSERT INTO Grade_Category VALUES ('GC000007', '64930', 'Homework', 30.00);
+INSERT INTO Grade_Category VALUES ('GC000008', '64930', 'Final Project', 60.00);
+-- grade category (discrete struc)
+INSERT INTO Grade_Category VALUES ('GC000009', '39285', 'Participation', 10.00);
+INSERT INTO Grade_Category VALUES ('GC000010', '39285', 'Homework', 30.00);
+INSERT INTO Grade_Category VALUES ('GC000011', '39285', 'Tests', 40.00);
+INSERT INTO Grade_Category VALUES ('GC000012', '39285', 'Final Exam', 20.00);
 
--- student record
-INSERT INTO Student_Record VALUES ('R0000001', 'S0000001', '10001', 95, 88, 90, 85, 92, NULL, NULL, NULL, 91, 5);
-INSERT INTO Student_Record VALUES ('R0000002', 'S0000002', '10001', 80, 75, 78, 70, 82, NULL, NULL, NULL, 85, 0);
-INSERT INTO Student_Record VALUES ('R0000003', 'S0000003', '10001', 70, 65, 72, 68, 74, NULL, NULL, NULL, 78, 3);
-INSERT INTO Student_Record VALUES ('R0000004', 'RP031006', '10001', 88, 90, 85, 92, 87, NULL, NULL, NULL, 89, 0);
-INSERT INTO Student_Record VALUES ('R0000005', 'NW050506', '10001', 76, 80, 78, 74, 82, NULL, NULL, NULL, 83, 2);
+-- enrolled in (database systems)
+INSERT INTO Enrolled_In VALUES ('NW050506', '23491');
+INSERT INTO Enrolled_In VALUES ('RP031006', '23491');
+INSERT INTO Enrolled_In VALUES ('HH230371', '23491');
+-- enrolled in (game dev)
+INSERT INTO Enrolled_In VALUES ('HH230371', '64930');
+INSERT INTO Enrolled_In VALUES ('MG238095', '64930');
+-- enrolled in (discrete struc)
+INSERT INTO Enrolled_In VALUES ('JK129376', '39285');
 
--- enrolled in
-INSERT INTO Enrolled_In VALUES ('S0000001', '10001');
-INSERT INTO Enrolled_In VALUES ('S0000002', '10001');
-INSERT INTO Enrolled_In VALUES ('S0000003', '10002');
-INSERT INTO Enrolled_In VALUES ('RP031006', '10001');
-INSERT INTO Enrolled_In VALUES ('NW050506', '10001');
->>>>>>> 752400985e30315c36e93eb6db056a410212c515
+-- student record (database systems)
+INSERT INTO Student_Record VALUES ('R0000001', 'NW050506', '23491', 96, 92, 94, 90, 88, NULL, NULL, NULL, NULL, 2);
+INSERT INTO Student_Record VALUES ('R0000002', 'RP031006', '23491', 75, 10, 0, 50, 45, NULL, NULL, NULL, NULL, 0);
+INSERT INTO Student_Record VALUES ('R0000003', 'HH230371', '23491', 89, 84, 86, 82, 85, NULL, NULL, NULL, NULL, 1);
+-- student record (game development)
+INSERT INTO Student_Record VALUES ('R0000004', 'HH230371', '64930', 93, NULL, 89, NULL, NULL, NULL, NULL, NULL, 95, 2);
+INSERT INTO Student_Record VALUES ('R0000005', 'MG238095', '64930', 97, NULL, 94, NULL, NULL, NULL, NULL, NULL, 98, 1);
+-- student record (discrete structures)
+INSERT INTO Student_Record VALUES ('R0000006', 'JK129376', '39285', 88, NULL, 91, 84, 86, NULL, NULL, NULL, NULL, 0);
+
+SELECT * FROM Student_Record;
